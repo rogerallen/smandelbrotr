f4f57fb66a49308831341b68774e431f24c36142
Roger Allen <rallen@gmail.com>
2019-06-28 15:28:31 -0700

    Separate out cuda kernels

---
+++ mandelbrot_kernels.h
@@ -0,0 +1,9 @@
+#ifndef SMANDELBROTR_MANDELBROT_KERNELS_H
+#define SMANDELBROTR_MANDELBROT_KERNELS_H
+
+#include <cuda.h>
+
+__global__ void mandel_float(uchar4 *ptr, int max_w, int max_h, int w, int h, float cx, float cy, float zoom, int iter_mult);
+__global__ void mandel_double(uchar4 *ptr, int max_w, int max_h, int w, int h, double cx, double cy, double zoom, int iter_mult);
+
+#endif
