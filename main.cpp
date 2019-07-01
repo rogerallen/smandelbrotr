55ae9fa4791942dad367d3f19c09079881a14dd4
Roger Allen <rallen@gmail.com>
2019-06-30 17:36:20 -0700

    fixup usage message a bit.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 void usage()
 {
     std::cout << "smandelbrotr - SDL2 CUDA OpenGL Mandelbrot explorer.\n";
-    std::cout << "\n";
+    std::cout << "             - by Roger Allen (rallen@gmail.com)\n";
+    std::cout << "             - https://github.com/rogerallen/smandelbrotr\n";
     std::cout << "options:\n";
-    std::cout << "  -d N - select cuda device number N. (default = 0)\n";
-    std::cout << "  -p PATH - path to shaders & compute kernel. (default = '.')\n";
-    std::cout << "  -h - this message.\n";
+    std::cout << "  -d N       - select cuda device number N. (default = 0)\n";
+    std::cout << "  -p PATH    - path to shaders & compute kernel. (default = '.')\n";
+    std::cout << "               (typically this should be the source directory)\n";
+    std::cout << "  -h         - this message.\n";
 }
 
 int main(int argc, char *argv[])
@@ -21,6 +23,7 @@ int main(int argc, char *argv[])
         if (argv[i][0] == '-') {
             if (argv[i][1] == 'h') {
                 usage();
+                return 0;
             }
             else if (argv[i][1] == 'd') {
                 cudaDevice = std::stoi(argv[++i]);
